2cd63c31e888da9220ac51d8dbf891c2c4e242de
PsybrKid <PsybrKid@KAVERI>
2016-04-26 02:49:28 -0400

    placed play file notification in try catch as it understandably fails when audio device is unconnected

--- SoundSwitch/Framework/NotificationManager/Notification/NotificationSound.cs
+++ SoundSwitch/Framework/NotificationManager/Notification/NotificationSound.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using AudioEndPointControllerWrapper;
@@ -8,55 +9,60 @@ using SoundSwitch.Framework.Audio;
 
 namespace SoundSwitch.Framework.NotificationManager.Notification
 {
-    public class NotificationSound : INotification
-    {
-        private readonly MMDeviceEnumerator _deviceEnumerator;
-        private readonly Stream _memoryStreamSound;
+	public class NotificationSound : INotification
+	{
+		private readonly MMDeviceEnumerator _deviceEnumerator;
+		private readonly Stream _memoryStreamSound;
 
-        public NotificationSound(Stream memoryStreamSound)
-        {
-            _memoryStreamSound = memoryStreamSound;
-            _deviceEnumerator = new MMDeviceEnumerator();
-        }
+		public NotificationSound(Stream memoryStreamSound)
+		{
+			_memoryStreamSound = memoryStreamSound;
+			_deviceEnumerator = new MMDeviceEnumerator();
+		}
 
-        private Stream GetStreamCopy()
-        {
-            lock (this)
-            {
-                _memoryStreamSound.Position = 0;
-                var memoryStreamedSound = new MemoryStream();
-                _memoryStreamSound.CopyTo(memoryStreamedSound);
-                memoryStreamedSound.Position = 0;
-                return memoryStreamedSound;
-            }
-        }
+		private Stream GetStreamCopy()
+		{
+			lock (this)
+			{
+				_memoryStreamSound.Position = 0;
+				var memoryStreamedSound = new MemoryStream();
+				_memoryStreamSound.CopyTo(memoryStreamedSound);
+				memoryStreamedSound.Position = 0;
+				return memoryStreamedSound;
+			}
+		}
 
-        public void NotifyDefaultChanged(IAudioDevice audioDevice)
-        {
-            if (audioDevice.Type != AudioDeviceType.Playback)
-                return;
+		public void NotifyDefaultChanged(IAudioDevice audioDevice)
+		{
+			if (audioDevice.Type != AudioDeviceType.Playback)
+				return;
 
-            var task = new Task(() =>
-            {
-                using (var memoryStreamedSound = GetStreamCopy())
-                {
-                    var device = _deviceEnumerator.GetDevice(audioDevice.Id);
-                    using (var output = new WasapiOut(device, AudioClientShareMode.Shared, true, 10))
-                    {
-                        output.Init(new WaveFileReader(memoryStreamedSound));
-                        output.Play();
-                        while (output.PlaybackState == PlaybackState.Playing)
-                        {
-                            Thread.Sleep(500);
-                        }
-                    }
-                }
-            });
-            task.Start();
-        }
+			var task = new Task(() =>
+			{
+				using (var memoryStreamedSound = GetStreamCopy())
+				{
+					var device = _deviceEnumerator.GetDevice(audioDevice.Id);
+					using (var output = new WasapiOut(device, AudioClientShareMode.Shared, true, 10))
+					{
+						try
+						{
+							output.Init(new WaveFileReader(memoryStreamedSound));
+							output.Play();
+							while (output.PlaybackState == PlaybackState.Playing)
+							{
+								Thread.Sleep(500);
+							}
+						}
+						catch
+						{ }
+					}
+				}
+			});
+			task.Start();
+		}
 
-        public void OnSoundChanged(CachedSound newSound)
-        {
-        }
-    }
+		public void OnSoundChanged(CachedSound newSound)
+		{
+		}
+	}
 }
